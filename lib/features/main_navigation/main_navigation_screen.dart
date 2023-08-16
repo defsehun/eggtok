f8f84a9654866b80149d92dce64b102569363eb7
defsehun <defsehun@gmail.com>
2023-08-16 22:56:14 +0900

    feat: main navigation

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:street_workout/constants/gaps.dart';
+import 'package:street_workout/constants/sizes.dart';
+import 'package:street_workout/features/main_navigation/widgets/nav_tab.dart';
+import 'package:street_workout/features/main_navigation/widgets/post_video_button.dart';
 
 class MainNavigaionScreen extends StatefulWidget {
   const MainNavigaionScreen({super.key});
@@ -13,19 +17,29 @@ class _MainNavigaionScreenState extends State<MainNavigaionScreen> {
 
   final screens = [
     const Center(
-      child: Text("Home"),
+      child: Text(
+        "Home",
+        style: TextStyle(fontSize: Sizes.size48),
+      ),
     ),
     const Center(
-      child: Text("Search"),
+      child: Text(
+        "Discover",
+        style: TextStyle(fontSize: Sizes.size48),
+      ),
+    ),
+    Container(),
+    const Center(
+      child: Text(
+        "Inbox",
+        style: TextStyle(fontSize: Sizes.size48),
+      ),
     ),
     const Center(
-      child: Text("Search"),
-    ),
-    const Center(
-      child: Text("Search"),
-    ),
-    const Center(
-      child: Text("Search"),
+      child: Text(
+        "Profile",
+        style: TextStyle(fontSize: Sizes.size48),
+      ),
     ),
   ];
 
@@ -35,30 +49,67 @@ class _MainNavigaionScreenState extends State<MainNavigaionScreen> {
     });
   }
 
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: const Text("Record video"),
+          ),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onTap,
-        destinations: const [
-          NavigationDestination(
-            icon: FaIcon(
-              FontAwesomeIcons.house,
-              color: Colors.blue,
-            ),
-            label: "Home",
+      body: screens.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
+                onTap: () => _onTap(1),
+              ),
+              Gaps.h24,
+              GestureDetector(
+                onTap: _onPostVideoButtonTap,
+                child: const PostVideoButton(),
+              ),
+              Gaps.h24,
+              NavTab(
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                onTap: () => _onTap(4),
+              ),
+            ],
           ),
-          NavigationDestination(
-            icon: FaIcon(
-              FontAwesomeIcons.magnifyingGlass,
-              color: Colors.blue,
-            ),
-            label: "Search",
-          ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:street_workout/constants/sizes.dart';
+
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          right: Sizes.size20,
+          child: Container(
+            height: Sizes.size32,
+            width: Sizes.size26,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: const Color(0xff61D4F0),
+              borderRadius: BorderRadius.circular(Sizes.size8),
+            ),
+          ),
+        ),
+        Positioned(
+          left: Sizes.size20,
+          child: Container(
+            height: Sizes.size32,
+            width: Sizes.size26,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(Sizes.size8),
+            ),
+          ),
+        ),
+        Container(
+          height: Sizes.size32,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size12,
+          ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              )),
+          child: const Center(
+            child: FaIcon(
+              FontAwesomeIcons.plus,
+              color: Colors.black,
+              size: Sizes.size20,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
