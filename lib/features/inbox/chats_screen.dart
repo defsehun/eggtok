3f85194841b5a26101b8fd38d1f64b418cf993f5
defsehun <defsehun@gmail.com>
2023-08-22 17:05:14 +0900

    feat: AnimatedList

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:street_workout/constants/sizes.dart';
+import 'package:street_workout/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -10,6 +11,76 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+  final List<int> _items = [];
+  final Duration _duration = const Duration(milliseconds: 300);
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(0, duration: _duration);
+      _items.add(_items.length);
+    }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(
+            color: Colors.red,
+            child: _makeTile(index),
+          ),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onTap: _onChatTap,
+      onLongPress: () => _deleteItem(index),
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+          "https://avatars.githubusercontent.com/u/17242597?v=4",
+        ),
+        child: Text("S"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "Namhee ($index)",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text("Don't forget to make money"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,46 +89,26 @@ class _ChatsScreenState extends State<ChatsScreen> {
         title: const Text("Direct messages"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
             ),
           ),
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/17242597?v=4",
-              ),
-              child: Text("S"),
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: _makeTile(index),
             ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  "Namhee",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  "2:16 PM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size12,
-                  ),
-                ),
-              ],
-            ),
-            subtitle: const Text("Don't forget to make money"),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
