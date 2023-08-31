217569559e3b1f4e06e8a1b166b15d74a6354183
defsehun <defsehun@gmail.com>
2023-08-31 16:35:20 +0900

    add camera awesome

--- lib/router.dart
+++ lib/router.dart
@@ -1,11 +1,11 @@
 import 'package:go_router/go_router.dart';
-import 'package:street_workout/features/videos/video_recording_screen.dart';
+import 'package:street_workout/features/videos/camera_awesome_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
       path: "/",
-      builder: (context, state) => const VideoRecordingScreen(),
+      builder: (context, state) => const CameraAwesomeScreen(),
     )
   ],
 );
