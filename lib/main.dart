9e49bfa834c8e8f04261513119c08a8328a77a1e
defsehun <defsehun@gmail.com>
2023-09-03 15:52:39 +0900

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -6,11 +7,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:street_workout/constants/sizes.dart';
 import 'package:street_workout/features/videos/repos/playback_config_repo.dart';
 import 'package:street_workout/features/videos/view_models/playback_config_vm.dart';
+import 'package:street_workout/firebase_options.dart';
 import 'package:street_workout/generated/l10n.dart';
 import 'package:street_workout/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await SystemChrome.setPreferredOrientations(
     [
